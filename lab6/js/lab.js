e5859ea984f3a12c8f6630f47b309026d6ed7dff
daisyl68 <dly11@ucsc.edu>
2025-04-28 18:53:02 -0700

    Lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Task 2
+// JavaScript Console with Data About Transportation
+// Daisy Ly
+// April 2025
 
-// Constants
+// Define Variables
+myTransport = ["Honda Odyssey", " Santa Cruz buses", " rides from friends", " walking"];
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Create an object for my main ride
+myMainRide = {
+  make : "Honda",
+  model : "Odyssey",
+  color : "Black",
+  year : 2016,
+  // we can define a function within our object (called a method)
+  // that uses the value of year above (using this.year)
+  age : function() {
+    return 2025 - this.year;
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// output
+document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+// this little trick allows us to write an object to the document
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
