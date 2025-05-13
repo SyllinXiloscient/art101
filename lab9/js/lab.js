de84a41fceb0fa4026a4d785b46117c0b1a2562b
daisyl68 <dly11@ucsc.edu>
2025-05-12 17:42:09 -0700

    Lab 9 Button Edits

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -5,21 +5,7 @@
     May 2025
 */
 
-// Add button to challenge section
-$("#challenge").append("The challenge of this lab was to create buttons and experiment with jQuery, and create and style our Lab 9 pages.");
-
-// Add button to problems section
-$("#problems").append("We both had problems trying to make the buttons do something.");
-
-
-// Add button to reflection section
-$("#reflection").append("My partner, Helwa Halloum, and I met up at RCC dining hall at 2 PM on Monday, May 12th, to work on the lab together.");
-
-// Add button to results section
-$("#results").append("View the results on this page!");
-
-
-// Add a click listener to the challenge button
+// Add a click listener to the buttons
 $("#button-challenge").click(function(){
     // Now add (or subtract) the "special" class to the section
     $("#challenge").toggleClass("special");
