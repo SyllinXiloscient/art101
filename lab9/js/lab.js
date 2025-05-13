21c2370debb2e4cc362c4066ad9169ee40050902
daisyl68 <dly11@ucsc.edu>
2025-05-12 21:14:46 -0700

    Bonus 3-in-1 jQuery Selector and Method Call Edit

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -5,20 +5,6 @@
     May 2025
 */
 
-// Add a click listener to the buttons
-$("#button-challenge").click(function(){
-    // Now add (or subtract) the "special" class to the section
-    $("#challenge").toggleClass("special");
-})
-
-$("#button-problems").click(function(){
-    $("#problems").toggleClass("special");
-})
-
-$("#button-reflection").click(function(){
-    $("#reflection").toggleClass("special");
-})
-
-$("#button-results").click(function(){
-    $("#results").toggleClass("special");
+$(".special-section button").click(function(){
+    $(this).parent().toggleClass("special");
 })
