d14d7a6e4f57ebfb4a8e1007ed100c490a4c1d64
daisyl68 <dly11@ucsc.edu>
2025-05-15 15:28:29 -0700

    Lab 10

---
+++ lab10/js/lab.js
@@ -0,0 +1,25 @@
+/*
+    Task 3
+    JavaScript - Append New Element to Output Div
+    Daisy Ly
+    May 2025
+*/
+
+function generateRandomText() {
+    const text = "Sebastian: My young master Ciel has another name. He's also known as the Queen's guard dog for his own in policing England's seedy underground. His small body houses great determination. You will marvel at the bravery he displays when dealing with the drug traffic in the ring. Also, feel free to admire my notable skills with the silver upon which I stake my honor as a Phantomhive butler. Next time on Black Butler, <i>His Butler, Strongest</i>. You see, I am simply one hell of a butler.";
+    const min = 3;
+    const max = 100;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    // Get a random starting index to slice the Lorem Ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    // Generate the random Lorem Ipsum-like text
+    return text.slice(randStart, randStart + randLen);
+}
+
+// Click listener for button
+$("#make-convo").click(function(){
+    // Get new fake dialogue
+    const newText = generateRandomText();
+    // Append a new div to our output div
+    $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+});
