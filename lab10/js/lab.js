a82fb78eb3207446689789ae0ffa95f5b4ed301b
daisyl68 <dly11@ucsc.edu>
2025-05-15 17:30:50 -0700

    Lab 10 Bonus Task

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -23,3 +23,23 @@ $("#make-convo").click(function(){
     // Append a new div to our output div
     $("#output").append('<div class="text"><p>' + newText + '</p></div>');
 });
+
+
+function generateRandomText1() {
+    const text = document.getElementById('inputField').value;
+    const min = 3;
+    const max = 50;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    // Get a random starting index to slice the Lorem Ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    // Generate the random Lorem Ipsum-like text
+    return text.slice(randStart, randStart + randLen);
+}
+
+// Click listener for button
+$("#make-convo1").click(function(){
+    // Get new fake dialogue
+    const newText = generateRandomText1();
+    // Append a new div to our output div
+    $("#output1").append('<div class="text"><p>' + newText + '</p></div>');
+});
