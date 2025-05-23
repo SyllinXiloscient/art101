d2c0f3e216add3b70ddea5605f2a40f52c38a168
daisyl68 <dly11@ucsc.edu>
2025-05-22 22:40:32 -0700

    Lab 12 Bonus Task

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -12,19 +12,54 @@ function sortingHat(str) {
     // Use modulus (% operator) to get the remainder with 4: mod = length % 4;
     let mod = length % 4;
     // Create a conditional that will return one of four iconic houses depending on whether the value of mod is 0, 1, 2, or 3
-    if (mod ==0) {
+    if (mod == 0) {
         return "Gryffindor"
     }
     else if (mod == 1) {
-        return "Slytherin"
+        return "Hufflepuff"
     }
     else if (mod == 2) {
-        return "Hufflepuff"
+        return "Slytherin"
     }
     else if (mod == 3) {
         return "Ravenclaw"
-  }
+    }
 }
+
+
+function getHouseDescription(house) {
+    let description;
+    if (house === 'Gryffindor') {
+        description = 'You belong in Gryffindor, where dwell the brave at heart. Their daring, nerve and chivalry set Gryffindors apart.';
+    } else if (house === 'Slytherin') {
+        description = 'You belong in Slytherin, where you’ll make your real friends. Those cunning folk use any means to achieve their ends.';
+    } else if (house === 'Hufflepuff') {
+        description = 'You belong in Hufflepuff, where they are just and loyal. Those patient Hufflepuffs are true and unafraid of toil.';
+    } else if (house === "Ravenclaw") {
+        description = 'You belong in wise old Ravenclaw with your ready mind, where those of wit and learning always find their kind.';
+    }
+   return description;
+}
+
+function getHouseCrest(des) {
+    let img;
+    if (des === 'You belong in Gryffindor, where dwell the brave at heart. Their daring, nerve and chivalry set Gryffindors apart.') {
+        img = document.createElement('img');
+        img.src = './img/gryffindor-crest.png';
+    } else if (des === 'You belong in Slytherin, where you’ll make your real friends. Those cunning folk use any means to achieve their ends.') {
+        img = document.createElement('img');
+        img.src = './img/slytherin-crest.png';
+    } else if (des === 'You belong in Hufflepuff, where they are just and loyal. Those patient Hufflepuffs are true and unafraid of toil.') {
+        img = document.createElement('img');
+        img.src = './img/hufflepuff-crest.png';
+    } else if (des === 'You belong in wise old Ravenclaw with your ready mind, where those of wit and learning always find their kind.') {
+        img = document.createElement('img');
+        img.src = './img/ravenclaw-crest.png';
+    }
+   return img;
+}
+
+let cred = "<i>(Harry Potter and the Philosopher's Stone. Edited for grammar purposes.)</i>"
 
 // Create a click listener attached to #button
 $("#button").click(function(){
@@ -32,8 +67,15 @@ $("#button").click(function(){
 let name = $("#input").val();
 // Run sortingHat(name) and store the result in a variable house
 let house = sortingHat(name);
+// Run getHouseDescription(house) and store the result in a variable house
+let des = getHouseDescription(house);
+// Run getHouseCrest(description) and store the result in a variable house
+let crest = getHouseCrest(des);
 // Append a new styled paragraph to #output that says "The Sorting Hat has sorted you into " + house
 $("#output").html("The Sorting Hat has sorted you into " + "<h5>" + house + "!!!" + "</h5>");
+$("#output2").html(des);
+$("#output4").html(crest);
+$("#output3").html(cred);
 });
 
 
